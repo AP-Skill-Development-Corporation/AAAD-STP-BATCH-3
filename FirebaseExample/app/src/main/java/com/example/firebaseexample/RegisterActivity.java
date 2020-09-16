bc8f0655475867abc2043ea3a91fef5bc87e4491
chaitanya <chaitanyakarri963@gmail.com>
2020-09-16 16:43:04 +0530

    16-09-2020

--- FirebaseExample/app/src/main/java/com/example/firebaseexample/RegisterActivity.java
+++ FirebaseExample/app/src/main/java/com/example/firebaseexample/RegisterActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -15,43 +14,38 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
-
-    //This is global declaration
+public class RegisterActivity extends AppCompatActivity {
     EditText email,pass;
     FirebaseAuth auth;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
         email = findViewById(R.id.email);
         pass = findViewById(R.id.pass);
         auth = FirebaseAuth.getInstance();
     }
 
-    public void login(View view) {
-        String uemail = email.getText().toString();
+    public void register(View view) {
+        String umail = email.getText().toString();
         String upass = pass.getText().toString();
-        if (uemail.isEmpty()|upass.isEmpty()){
+        if (umail.isEmpty()|upass.isEmpty()){
             Toast.makeText(this, "Fill all the details", Toast.LENGTH_SHORT).show();
         }
         else{
-            auth.signInWithEmailAndPassword(uemail,upass).addOnCompleteListener(this,
+            auth.createUserWithEmailAndPassword(umail,upass).addOnCompleteListener(this,
                     new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()){
-                                Log.i("Apssdc","Login Successful");
+                                Toast.makeText(RegisterActivity.this, "Successfull", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(RegisterActivity.this,MainActivity.class));
+                                finish();
                             }else{
-                                Log.i("Apssdc","Login Failed");
+                                Toast.makeText(RegisterActivity.this, "Failed", Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
         }
-    }
-
-    public void register(View view) {
-        startActivity(new Intent(this,RegisterActivity.class));
     }
 }
