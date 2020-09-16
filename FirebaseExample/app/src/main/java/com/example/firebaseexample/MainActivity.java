e9731acae19cd7a1bf0ea8d06586465d90fce429
chaitanya <chaitanyakarri963@gmail.com>
2020-09-16 17:34:40 +0530

    16-09-2020

--- FirebaseExample/app/src/main/java/com/example/firebaseexample/MainActivity.java
+++ FirebaseExample/app/src/main/java/com/example/firebaseexample/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.firebaseexample;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -53,5 +56,42 @@ public class MainActivity extends AppCompatActivity {
 
     public void register(View view) {
         startActivity(new Intent(this,RegisterActivity.class));
+    }
+
+    public void reset(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View v = LayoutInflater.from(this).inflate(R.layout.reset,null,false);
+        final EditText mail = v.findViewById(R.id.email);
+        builder.setView(v);
+        builder.setCancelable(false);
+        builder.setPositiveButton("Reset", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String umail = mail.getText().toString();
+                if (umail.isEmpty()){
+                    Toast.makeText(MainActivity.this, "cant be empty", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    auth.sendPasswordResetEmail(umail).addOnCompleteListener(MainActivity.this, new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(MainActivity.this, "Reset mail sent", Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                Toast.makeText(MainActivity.this, "Failed to reset", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
     }
 }
